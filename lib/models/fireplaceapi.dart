d483ad8440ad451d1b1a2daaff4e2cb451cfa492
annaholopainen <65926354+annaholopainen@users.noreply.github.com>
2023-04-18 11:06:00 +0300

    set up the fireplace api correctly

--- lib/models/fireplaceapi.dart
+++ lib/models/fireplaceapi.dart
@@ -2,17 +2,19 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:energyapp_v3/views/widgets/fireplaceadvice.dart';
 
-class FireplaceAPI{
-  Future<FireplaceAdvice> getFireplaceAdvise() async {
+class FireplaceAdviceAPI{
+  Future<FireplaceAdvice> getFireplaceAdvice() async {
     final response = await http.get(
         Uri.parse('http://10.0.2.2:8000/api/advise_firewood_warming')
     );
     if (response.statusCode == 200) {
-      final data = jsonDecode(response.body);
-      final cutstring = data['Advise'].replaceAll('{afw}', '');
-      final fireplaceAdvice = FireplaceAdvice(advice: cutstring, category: data['Category']);
-      return fireplaceAdvice;
-    } else {
-      throw Exception('Failed to load data');
+        final data = jsonDecode(response.body);
+        final fireplaceAdvice = FireplaceAdvice(
+            value: data["Advise"].toString().substring(0, data["Advise"].toString().lastIndexOf(' ')),
+            category: data["Category"]
+        );
+        return fireplaceAdvice;
+      } else {
+        throw Exception('Failed to load data');
     }
   }}
